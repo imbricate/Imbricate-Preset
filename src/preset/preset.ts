5c59fc0e368299e11dc11a00b56813c231de4e96
wmxpy <wm@mengw.io>
2024-02-20 14:49:10 -0600

    Refactor code to improve performance and readability

---
+++ src/preset/preset.ts
@@ -0,0 +1,17 @@
+/**
+ * @author WMXPY
+ * @namespace Preset
+ * @description Preset
+ */
+
+export class ImbricatePreset {
+
+    public static fromScratch(): ImbricatePreset {
+
+        return new ImbricatePreset();
+    }
+
+    private constructor() {
+
+    }
+}
---
+++ src/preset/rule.ts
@@ -0,0 +1,17 @@
+/**
+ * @author WMXPY
+ * @namespace Preset
+ * @description Rule
+ */
+
+export class ImbricatePresetRule {
+
+    public static fromScratch(): ImbricatePresetRule {
+
+        return new ImbricatePresetRule();
+    }
+
+    private constructor() {
+
+    }
+}
